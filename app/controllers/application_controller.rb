4e03baeb465492f7342b94db5af68bae858228a7
kpahawa <k661993P>
2016-02-21 15:13:55 -0500

    updated a bunch of images and added the main pictures. look into making the laod time faster.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,18 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  # def current_controller?(names)
-  #   names.include?(current_controller)
-  # end
+  @insert_pics = true
+  def self.initial_update(i)
+    if i
+        j = 1
+        while j < 38
+          url = "/assets/p#{j}.JPG"
+          Picture.find_or_create_by( :title=> "p#{j}", :url => url)
+          j += 1
+        end
+        @insert_pics = false
+    end
+  end
+
+  self.initial_update(@insert_pics)
 end
